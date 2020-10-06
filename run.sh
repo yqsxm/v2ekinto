a1c742ecb98e7abab0d5a13f6823d196511ed5d7
yeahwu <yeahwu@users.noreply.github.com>
2020-10-06 22:24:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,62 @@
 #!/bin/sh
 
-./v2ctl config config.txt > web.pb &&
-base64 web.pb > base.txt
+UUID="c95ef1d4-f3ac-4586-96e9-234a37dda068"
+PROTOCOL="vmess"
+WS_PATH="/"
+
+encode() {
+  arg1=$1
+  base64 $arg1 
+}
+
+cat > config.json << EOF
+{
+  "log": {
+    "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "port": 3000,
+            "protocol": "${PROTOCOL}",
+            "settings": {
+                "decryption": "none",
+                "clients": [
+                    {
+                        "id": "${UUID}"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network":"ws",
+                "wsSettings": {
+                    "path": "${WS_PATH}"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {}
+        }
+    ]
+}
+EOF
+
+v_config_pb=`./v2ctl config config.json | encode`
+rm config.json
+# Creat start.sh and then encrypt it with shc
+cat > start.sh << EOF
+#!/bin/sh
+p_config_pb="${v_config_pb}"
+echo \$p_config_pb | base64 -di > config.pb
+chmod 0755 ./v2ray
+./v2ray -config=./config.pb >/dev/null 2>/dev/null&
+sleep 5 ; rm ./config.pb
+sleep 999d
+EOF
+
+export CFLAGS=-static 
+shc -r -f  start.sh 
+rm start.sh.x.c start.sh 
+mv start.sh.x start
